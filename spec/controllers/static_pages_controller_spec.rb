e385d017c3a47cb9e2a31000d24d357f28bc8c99
tomas <hundred.spires@gmail.com>
2012-01-28 15:12:04 +0100

    Add a StaticPages controller

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe StaticPagesController do
+
+  describe "GET 'home'" do
+    it "returns http success" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'contact--no-test-framework'" do
+    it "returns http success" do
+      get 'contact--no-test-framework'
+      response.should be_success
+    end
+  end
+
+end
