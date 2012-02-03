0e74fff5da75152b4e8d63f263d5ff0362f7d7f1
tomas <hundred.spires@gmail.com>
2012-02-03 19:42:08 +0100

    Make a basic User model (including secure passwords)

--- spec/views/static_pages/contact.html.erb_spec.rb
+++ spec/views/static_pages/contact.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe "static_pages/contact--no-test-framework.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
